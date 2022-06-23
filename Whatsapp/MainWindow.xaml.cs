83fb998a561f070864191fd9320e5812c4cf507b
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-06-23 23:08:03 +0400

    0.5

--- Whatsapp/MainWindow.xaml.cs
+++ Whatsapp/MainWindow.xaml.cs
@@ -20,9 +20,79 @@ namespace Whatsapp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isok { get; set; } = true;
+        bool isokay = true;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private Label Label()
+        {
+            Label lbl = new Label();
+            lbl.FontSize = 18;
+            lbl.Content = TXTBOX.Text;
+            lbl.Foreground = new SolidColorBrush(Colors.White);
+            return lbl;
+        }
+        private void AddMessage()
+        {
+            int num = 10;
+            if (isok)
+            {
+                Label lv = new Label();
+                num = 0;
+                Border BB = new Border();
+                isok = false;
+                lv.FontSize = 16;
+                lv.Content = DateTime.Now;
+                lv.Foreground = new SolidColorBrush(Colors.White);
+                BB.Child = lv;
+                BB.Background = new SolidColorBrush(Color.FromRgb(16, 17, 27));
+                BB.HorizontalAlignment = HorizontalAlignment.Center;
+                BB.Child = lv;
+                BB.CornerRadius = new CornerRadius(10, 10, 10, 10);
+                MSGR.Children.Add(BB);
+            }
+            Border B = new Border();
+            B.CornerRadius = new CornerRadius(12,0,20,12);
+            B.HorizontalAlignment = HorizontalAlignment.Right;
+            B.Margin = new Thickness(num, num, 10, 0);
+            B.Background = new SolidColorBrush(Color.FromRgb(37, 211, 102));
+            B.Child = Label();
+            MSGR.Children.Add(B);
+            SV.ScrollToBottom();
+            Fun();
+        }
+        private void TextBox_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            
+            
+        }
+        private void Fun2()
+        {
+            TXTBOX.Text = "";
+            TXTBOX.Foreground = new SolidColorBrush(Colors.Black);
+        }
+        private void TXTBOX_MouseEnter(object sender, MouseEventArgs e) { Fun2(); }
+        private void Fun()
+        {
+            
+                TXTBOX.Text = "Type a message";
+                TXTBOX.Foreground = new SolidColorBrush(Color.FromRgb(197, 197, 197));
+                isokay = true;
+            
+        }
+        private void TXTBOX_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (TXTBOX.Text.Length==0)Fun();
+        }
+
+        private void TXTBOX_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.Key == Key.Enter && TXTBOX.Text.Length > 0) AddMessage();
+            else if (TXTBOX.Text.Length == 0) { TXTBOX.Text = "Type a message"; }
+            else if (isokay) { TXTBOX.Text = ""; Fun2(); isokay = false; }
         }
     }
 }
